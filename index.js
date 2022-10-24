edd468a0f90bb9140fa2a1f468d497a7e6c9011d
Pintu-Roy121 <roypintu121@gmail.com>
2022-10-25 03:46:54 +0600

    couse Category added

--- index.js
+++ index.js
@@ -6,16 +6,45 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const course = require('./data/courses.json')
+const category = require('./data/coursesCategory.json')
+const allCategory = require('./data/allCourses.json')
 
 app.get('/', (req, res) => {
-    res.send('EduCare server is running now');
+    res.send('EduCare server is running update');
 });
 
 app.get('/course', (req, res) => {
-    res.send(course);
+    res.send(category);
 })
 
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const categori = category.find(cg => cg.id == id);
+
+    res.send(categori);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+
+    const selectedProduct = allCategory.filter(catagory => catagory.category_id === id)
+
+    res.send(selectedProduct);
+})
+
+// app.get('/category/:id', (req, res) => {
+//     const id = req.params.id;
+
+//     if (id === '08') {
+//         res.send(news);
+//     }
+//     else {
+//         const category_News = news.filter(n => n.category_id === id);
+//         res.send(category_News);
+//     }
+// })
 
 app.listen(port, () => {
     console.log(`Educare is running on Port ${port}`);
