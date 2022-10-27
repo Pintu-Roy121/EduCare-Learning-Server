73e4104612ab1c251cd0a6206b924fe601f0ab1d
Pintu-Roy121 <roypintu121@gmail.com>
2022-10-27 08:05:01 +0600

    final commit

--- index.js
+++ index.js
@@ -13,42 +13,36 @@ app.get('/', (req, res) => {
     res.send('EduCare server is running');
 });
 
-app.get('/category', (req, res) => {
+app.get('/allcategory', (req, res) => {
     res.send(allCategory);
-})
-
-app.get('/course/:id', (req, res) => {
-    const id = req.params.id;
-
-    const category = allCategory.find(category => category.id === id);
-
-    res.send(category);
-})
-
-app.get('/allcourses', (req, res) => {
-    res.send(allCourse);
 })
 
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
-    console.log(id);
+    const category = allCategory.find(category => category.id === id);
+    res.send(category);
+})
 
-    const selectedCourse = allCourse.filter(course => course.category_id === id)
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const SelectedCourse = allCourse.find(course => course._id === id)
+    res.send(SelectedCourse);
+})
 
+app.get('/allcourses', (req, res) => {
+    res.send(allCourse)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id === '08') {
+        res.send(allCourse)
+    }
+    const selectedCourse = allCourse.filter(course => course.category_id == id)
     res.send(selectedCourse);
 })
 
-// app.get('/category/:id', (req, res) => {
-//     const id = req.params.id;
-
-//     if (id === '08') {
-//         res.send(news);
-//     }
-//     else {
-//         const category_News = news.filter(n => n.category_id === id);
-//         res.send(category_News);
-//     }
-// }) 
 app.listen(port, () => {
     console.log(`Educare is running on Port ${port}`);
 })
